ff5298e1fbaa3d807e1202f40b505844e1581ec6
Christopher Wray <chris@solmediaco.com>
2022-05-28 20:49:04 -0700

    responsivy updates to hero

--- frontend/components/Sections/Hero.tsx
+++ frontend/components/Sections/Hero.tsx
@@ -18,12 +18,12 @@ function HeroSection({ title = "Simple Ideas for Fun" }: Props) {
             alt="decoration"
           />
         </div>
-        <div className="relative px-4 xl:px-0 container mx-auto grid grid-cols-7 items-center gap-16">
-          <div className="text-color w-full col-span-3 pt-16 lg:pt-32 xl:pt-12">
+        <div className="relative px-4 xl:px-0 container mx-auto grid md:grid-cols-7 items-center gap-x-16 gap-y-6">
+          <div className="text-color w-full md:col-span-3 pt-16 lg:pt-32 xl:pt-12">
             <h1 className="text-4xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full xl:text-6xl text-gray-900 font-extrabold f-f-l">
               {title}
             </h1>
-            <div className="f-f-r text-base lg:text-base pb-20 sm:pb-0 pt-10 xl:pt-6">
+            <div className="f-f-r text-base lg:text-base pt-10 xl:pt-6">
               <h2>By Areli Wray</h2>
             </div>
             <div className="lg:flex">
@@ -42,14 +42,14 @@ function HeroSection({ title = "Simple Ideas for Fun" }: Props) {
             </div>
           </div>
           <img
-            className="w-full col-span-4 mt-8 md:mt-0 object-fill md:-ml-4 lg:-ml-4 xl:ml-0"
+            className="w-full rounded shadow-sm md:col-span-4 mt-8 md:mt-0 object-fill md:-ml-4 lg:-ml-4 xl:ml-0"
             src="/book-picture-simple-ideas-for-fun.jpg"
             alt="sample page"
           />
-          <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-6/12 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
+          <button className="md:hidden hover:opacity-90 text-base w-full f-f-r py-4  bg-indigo-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
             Start building now
           </button>
-          <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-4/12 xl:ml-2 mt-4 xl:mt-8 f-f-r py-4  bg-indigo-200 text-indigo-600 font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
+          <button className="md:hidden hover:opacity-90 text-base w-full f-f-r py-4  bg-indigo-200 text-indigo-600 font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 rounded-lg">
             Try it out
           </button>
         </div>
